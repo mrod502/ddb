330347fcc3263d353f764b8f56bf536d52492eec
mrod502 <mrod22502@gmail.com>
2020-10-27 17:27:41 -0400

    pre-branch commit

--- ddb.go
+++ ddb.go
@@ -1,10 +1,79 @@
 package ddb
 
-import "github.com/dgraph-io/badger/v2"
+import (
+	"encoding/json"
+	"net/http"
+	"time"
 
-var db *badger.DB
+	badger "github.com/dgraph-io/badger/v2"
+	"github.com/gorilla/mux"
+	"github.com/mrod502/logger"
+)
 
-func OpenDB() (err error) {
-	db, err = badger.Open(badger.DefaultOptions("/tmp/badger"))
+const ()
+
+var (
+	db     *badger.DB
+	config DBOptions
+)
+
+//OpenDB - open the database at path
+func OpenDB(options []byte) (err error) {
+	err = json.Unmarshal(options, &config)
+	if err != nil {
+		logger.Log("DB", err.Error())
+	}
+	db, err = badger.Open(badger.DefaultOptions(config.Path))
+
+	go func() {
+		for {
+			time.Sleep(time.Second * time.Duration(max(config.GCInterval, 60)))
+			db.RunValueLogGC(0.7)
+
+		}
+	}()
 	return
+}
+
+//DBOptions - options for configuring database
+type DBOptions struct {
+	Path       string
+	GCInterval int64
+	Servers    []string
+}
+
+func max(in ...int64) int64 {
+	var max = in[0]
+	for _, v := range in {
+		if v > max {
+			max = v
+		}
+	}
+	return max
+}
+
+//CloseDB - closes the db
+func CloseDB() {
+	err := db.Close()
+	if err != nil {
+		logger.Log("CloseDB", err.Error())
+	}
+}
+
+func Serve() {
+	var router = mux.NewRouter()
+	router.HandleFunc("/", handleRequests).Methods("POST")
+}
+
+func ServeDistributed() {}
+
+func handleRequests(w http.ResponseWriter, r *http.Request) {
+
+}
+
+type DBAction struct {
+	APIKey     string
+	ActionType int
+	Key        []byte
+	Value      []byte
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/mrod502/ddb
 
 go 1.15
 
-require github.com/dgraph-io/badger/v2 v2.2007.2
+require (
+	github.com/dgraph-io/badger/v2 v2.2007.2
+	github.com/mrod502/logger v0.0.0-20201027191409-c7e4cc425815
+)
